a5d08897312ad3dae1528c304d01f3002a0da988
mahfuzur-rahman-sazid <137096762+mahfuzur-rahman-sazid@users.noreply.github.com>
2023-06-20 04:27:08 +0600

    Add files via upload

---
+++ splash.h
@@ -0,0 +1,23 @@
+
+
+
+
+void Splash_Screen(void) {
+
+
+    printf("\n");
+    printf("  FFFFFFFFFFFFFF    OOOOOOOOO      OOOOOOOOO    DDDDDDDDDDDDD       \n");
+    printf("  FFFFFFFFFFFFFF   OOOOOOOOOOO    OOOOOOOOOOO   DDDDDDDDDDDDDD      \n");
+    printf("  FFFFF            OOOOO   OOOO  OOOO   OOOOOO  DDDD       DDDD     \n");
+    printf("  FFFFF            OOOO     OOO  OOO      OOOO  DDDD        DDDD    \n");
+    printf("  FFFFFFFFFF       OOOO     OOO  OOO      OOOO  DDDD        DDDD    \n");
+    printf("  FFFFFFFFFF       OOOO     OOO  OOO      OOOO  DDDD        DDDD    \n");
+    printf("  FFFFF            OOOO     OOO  OOO      OOOO  DDDD        DDDD    \n");
+    printf("  FFFFF            OOOO     OOO  OOO      OOOO  DDDD       DDDD     \n");
+    printf("  FFFFF            OOOOO   OOOO  OOOO    OOOOO  DDDDDDDDDDDDDD      \n");
+    printf("  FFFFF            OOOOOOOOOOO    OOOOOOOOOOO   DDDDDDDDDDDDD       \n");
+    printf("  FFFFF            OOOOOOOOO      OOOOOOOOO     DDDDDDDDDDDD        \n\n");
+    printf("            PRESENTING YOU! OUR VERY OWN FOOD ORDERING SYSTEM\n\n");
+    printf("                    ENJOY THE EXPERIENCE!\n\n");
+}
+
